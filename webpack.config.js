d4e345db5a2a77f68fd8d314e49a395eba21c839
Vasily-N <101352346+Vasily-N@users.noreply.github.com>
2022-05-08 16:39:48 +0300

    fix: eslint-config-airbnb-base for webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,45 +3,51 @@ const path = require('path');
 const HTMLWebPackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const sass = require('sass');
 
 const namePattern = '[name].[contenthash]';
-const isDev = true; //todo:
+const isDev = true; // todo:
 
 module.exports = {
-    mode: 'development',
-    entry: path.resolve(__dirname, 'src/index.js'),
-    output: {
-        filename: `${namePattern}.js`,
-        path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-        new HTMLWebPackPlugin({
-            template: path.resolve(__dirname, 'src/index.html'),
-        }),
-        new MiniCssExtractPlugin({
-            filename: `${namePattern}.css`,
-        }),
-        new CleanWebpackPlugin(),
-    ],
-    devServer: {
-        port: 5500,
-        hot: isDev,
-    },
-    devtool: isDev ? 'source-map' : '',
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader, 'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            implementation: require('sass'),
-                        },
-                    },
-                ],
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: `${namePattern}.js`,
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HTMLWebPackPlugin({
+      template: path.resolve(__dirname, 'src/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: `${namePattern}.css`,
+    }),
+    new CleanWebpackPlugin({
+      cleanOnceBeforeBuildPatterns: [
+        '**/*',
+        '!.git',
+      ],
+    }),
+  ],
+  devServer: {
+    port: 5500,
+    hot: isDev,
+  },
+  devtool: isDev ? 'source-map' : '',
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader, 'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: sass,
             },
-        ]
-    }
+          },
+        ],
+      },
+    ],
+  },
 };
